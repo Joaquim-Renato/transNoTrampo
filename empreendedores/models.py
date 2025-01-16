4bc3fb0c3cb5072ec306dc9a3077fcdd795b6709
Joaquim-Renato <jocajoaquimas@gmail.com>
2025-01-16 15:57:08 -0300

    id bd resolvido

--- empreendedores/models.py
+++ empreendedores/models.py
@@ -4,6 +4,7 @@ from django.db import models
 
 
 class Empreendedor(models.Model):
+    codigo = models.AutoField(primary_key=True) 
     nome = models.CharField(max_length=200)
     idade = models.CharField(max_length=4)
     identidadegenero = models.CharField(max_length=20)
