3c20beac364e8fac1ed3c5574c08d2a5778f3984
Joaquim <jocajoaquimas@gmail.com>
2025-01-16 18:40:29 -0300

    edit funcionando

--- empreendedores/urls.py
+++ empreendedores/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('cadastrar/', views.cadastrar_empreendedor, name='cadastrar_empreendedor'),
     path('lista/', views.lista_empreendedores, name='lista_empreendedores'),
     path('', views.index, name='index'),
-    path('editempreendedor', views.edit_empreendedor, name='edit_empreendedor')
+    path('editar/<int:id>/', views.edit_empreendedor, name='edit_empreendedor')
 ]
--- empreendedores/views.py
+++ empreendedores/views.py
@@ -40,3 +40,36 @@ def lista_empreendedores(request):
 def index(request):
     return render(request, 'index.html')  
 
+def edit_empreendedor(request, id):
+    empreendedor = Empreendedor.objects.get(id=id)
+    if request.method == 'POST':
+        nome = request.POST['nome']
+        idade = request.POST['idade']
+        identidadegenero = request.POST['identidadegenero']
+        email = request.POST['email']
+        telefone = request.POST['telefone']
+        servico = request.POST['servico']
+        descricao = request.POST['descricao']
+
+        if not nome or not email or not servico:
+            return render(
+                request, 
+                'editar.html', 
+                {'empreendedor': empreendedor, 'error': 'Preencha os campos obrigatórios.'}
+            )
+
+        empreendedor.nome = nome
+        empreendedor.idade = idade
+        empreendedor.identidadegenero = identidadegenero
+        empreendedor.email = email
+        empreendedor.telefone = telefone
+        empreendedor.servico = servico
+        empreendedor.descricao = descricao
+        empreendedor.save()
+
+        return redirect('lista_empreendedores')
+    
+    return render(request, 'editar.html', {'empreendedor': empreendedor})
+
+
+
