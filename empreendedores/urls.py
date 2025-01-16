b9bae80349a53f3f91f00bc1f9d436cf2407a2db
Joaquim-Renato <jocajoaquimas@gmail.com>
2025-01-16 15:30:01 -0300

    lista funcionando

--- empreendedores/urls.py
+++ empreendedores/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('cadastrar/', views.cadastrar_empreendedor, name='cadastrar_empreendedor'),
     path('lista/', views.lista_empreendedores, name='lista_empreendedores'),
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
 ]
