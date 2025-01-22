aa601714737824a7bbbed65f4c58021e895d0b9c
JOAQUIM RENATO ALVES DE SOUZA <joaquim.rasouza@senacsp.edu.br>
2025-01-22 14:32:25 -0300

    cadastro funcionando

--- empreendedores/models.py
+++ empreendedores/models.py
@@ -8,7 +8,7 @@ class Empreendedor(models.Model):
     identidadegenero = models.CharField(max_length=20)
     telefone = models.CharField(max_length=15)
     email = models.EmailField(unique=True)
-    senha = models.CharField(max_length=50)
+    senha = models.CharField(max_length=300)
     servico = models.CharField(max_length=200)
     descricao = models.TextField()
     data_cadastro = models.DateTimeField(auto_now_add=True)
