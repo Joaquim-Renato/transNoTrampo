36e824b88fe57f87516571eb290343e9c9db09f6
Joaquim-Renato <jocajoaqumas@gmail.com>
2025-01-24 16:30:35 -0300

    CRUD

--- empreendedores/forms.py
+++ empreendedores/forms.py
@@ -6,4 +6,6 @@ class EmpreendedorForm(forms.ModelForm):
     class Meta:
         model = Empreendedor
         fields = ['nome', 'idade', 'identidadegenero', 'telefone', 'email', 'senha', 'servico', 'descricao']
-
+        widgets = {
+            'senha': forms.PasswordInput(),  # Isso vai garantir que o campo senha seja exibido como '*****'
+        }
